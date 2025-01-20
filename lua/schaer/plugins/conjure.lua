be9b06ee9190c93007c3f087fcd42a2309278ecf
Rodrigo Schaer <rodrigo.schaer@rodrigo.schaer>
2025-01-20 11:35:19 -0300

    feat: updates conjure

--- lua/schaer/plugins/conjure.lua
+++ lua/schaer/plugins/conjure.lua
@@ -8,9 +8,6 @@ return {
 			-- Uncomment this to get verbose logging to help diagnose internal Conjure issues
 			-- This is VERY helpful when reporting an issue with the project
 			-- vim.g["conjure#debug"] = true
-
-			-- Rebind the documentation mapping from K to <prefix>gk
-			vim.g["conjure#mapping#doc_word"] = "<leader>gk"
 		end,
 
 		-- Optional cmp-conjure integration
@@ -20,10 +17,10 @@ return {
 		"PaterJason/cmp-conjure",
 		lazy = true,
 		config = function()
-			-- local cmp = require("cmp")
-			-- local config = cmp.get_config()
-			-- table.insert(config.sources, { name = "conjure" })
-			-- return cmp.setup(config)
+			local cmp = require("cmp")
+			local config = cmp.get_config()
+			table.insert(config.sources, { name = "conjure" })
+			return cmp.setup(config)
 		end,
 	},
 }
