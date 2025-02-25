33dc8a31e3127effbe818ca98ceaaa43c4305392
Rodrigo Schaer <rodrigo.fscs@gmail.com>
2025-02-24 21:33:00 -0300

    refactor: changed conjure.lua to clojure.lua

--- lua/schaer/plugins/clojure.lua
+++ lua/schaer/plugins/clojure.lua
@@ -1,7 +1,38 @@
 return {
-	"clojure-vim/vim-jack-in",
-	dependencies = {
-		"tpope/vim-dispatch",
-		"radenling/vim-dispatch-neovim"
+	{
+		"Olical/conjure",
+		ft = { "clojure", "fennel", "python" }, -- etc
+		lazy = true,
+		init = function()
+			-- Set configuration options here
+			-- Uncomment this to get verbose logging to help diagnose internal Conjure issues
+			-- This is VERY helpful when reporting an issue with the project
+			-- vim.g["conjure#debug"] = true
+
+			vim.g["conjure#mapping#prefix"] = ","
+		end,
+
+		-- Optional cmp-conjure integration
+		dependencies = { "PaterJason/cmp-conjure", "clojure-vim/vim-jack-in" },
 	},
+	{
+		"PaterJason/cmp-conjure",
+		lazy = true,
+		config = function()
+			local cmp = require("cmp")
+			local config = cmp.get_config()
+			table.insert(config.sources, { name = "conjure" })
+			return cmp.setup(config)
+		end,
+	},
+	{
+		"clojure-vim/vim-jack-in",
+		dependencies = {
+			"tpope/vim-dispatch",
+			"radenling/vim-dispatch-neovim"
+		},
+		config = function()
+			vim.keymap.set("n", "<leader>L", "<CMD>Lein<CR>", { desc = "Opens Lein REPL with Jack In" })
+		end
+	}
 }
