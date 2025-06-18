d3c964e971ecdbcee966168da55fc2fe322d7f3f
Rodrigo Schaer <rodrigo.fscs@gmail.com>
2025-06-18 16:03:33 -0300

    feat: improves paredit mappings

--- lua/schaer/plugins/paredit.lua
+++ lua/schaer/plugins/paredit.lua
@@ -1,12 +1,57 @@
 return {
 	"julienvincent/nvim-paredit",
+	dependencies = {
+		"tpope/vim-sexp-mappings-for-regular-people",
+		"tpope/vim-repeat"
+	},
 	config = function()
 		local paredit = require("nvim-paredit")
 		paredit.setup({
 			-- Change some keys
 			keys = {
-				["<leader>o"] = false,
-				["<leader>r"] = { paredit.api.raise_form, "Raise form" },
+				["<localleader>w("] = {
+					function()
+						-- place cursor and set mode to `insert`
+						paredit.cursor.place_cursor(
+						-- wrap element under cursor with `( ` and `)`
+							paredit.wrap.wrap_element_under_cursor("( ", ")"),
+							-- cursor placement opts
+							{ placement = "inner_start", mode = "insert" }
+						)
+					end,
+					"Wrap element insert head",
+				},
+
+				["<localleader>W("] = {
+					function()
+						paredit.cursor.place_cursor(
+							paredit.wrap.wrap_element_under_cursor("(", ")"),
+							{ placement = "inner_end", mode = "insert" }
+						)
+					end,
+					"Wrap element insert tail",
+				},
+
+				-- same as above but for enclosing form
+				["<localleader>i("] = {
+					function()
+						paredit.cursor.place_cursor(
+							paredit.wrap.wrap_enclosing_form_under_cursor("( ", ")"),
+							{ placement = "inner_start", mode = "insert" }
+						)
+					end,
+					"Wrap form insert head",
+				},
+
+				["<localleader>I("] = {
+					function()
+						paredit.cursor.place_cursor(
+							paredit.wrap.wrap_enclosing_form_under_cursor("(", ")"),
+							{ placement = "inner_end", mode = "insert" }
+						)
+					end,
+					"Wrap form insert tail",
+				}
 			},
 		})
 	end
